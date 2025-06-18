fae6119b904c4579dc201032541727880fcb2da4
Ofentse <ofentsemakhutja@icloud.com>
2025-06-18 09:34:20 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const app = express();
 require("dotenv").config();
+const auth = require("./routes/auth");
 
 connectDB();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", auth);
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/expenses", require("./routes/expenses"));
